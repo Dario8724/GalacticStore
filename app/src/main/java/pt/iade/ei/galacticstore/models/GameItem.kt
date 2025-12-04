a329f21c379bad17af40d680f6058b4606d7a3e9
Dário Bandanhe <dariobbandanhe@gmail.com>
2025-12-04 00:58:31 +0000

    calling it a day

--- app/src/main/java/pt/iade/ei/galacticstore/models/GameItem.kt
+++ app/src/main/java/pt/iade/ei/galacticstore/models/GameItem.kt
@@ -1,7 +1,14 @@
 package pt.iade.ei.galacticstore.models
 
+import android.R
+
 data class GameItem (
     // add items
+    //  search if i should use a var for the image
     var gameName : String,
-
+    var gameDescription : String,
+    var gameItemName : String,
+    var itemDescription : String,
+    var itemPrice : String
+    // add more things
 )
